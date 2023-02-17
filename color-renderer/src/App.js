6f725e0782f0ee298a6f95341751d1ca50a7cb9d
bt <btfranzen@gmail.com>
2023-02-16 18:45:43 -0700

    color-renderer complete

---
+++ color-renderer/src/App.js
@@ -0,0 +1,11 @@
+import ColorRenderer from "./Components/ColorRenderer";
+
+function App() {
+    return (
+        <div className='App'>
+            <ColorRenderer />
+        </div>
+    );
+}
+
+export default App;
