56467117cdf754f74d590ffbbc3033b99280ab7c
bt <btfranzen@gmail.com>
2023-02-17 22:31:38 -0700

    score keeper

---
+++ 5_score-keeper/src/App.js
@@ -0,0 +1,12 @@
+import ScoreKeeper from "./Components/ScoreKeeper";
+import "./App.css";
+
+function App() {
+    return (
+        <div className='App'>
+            <ScoreKeeper />
+        </div>
+    );
+}
+
+export default App;
