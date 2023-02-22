35b88de0e35ca2632c3fe4bbfa994a5dd106e417
bt <btfranzen@gmail.com>
2023-02-21 20:48:52 -0700

    color-picker completed

--- 7_color-picker/src/Components/Color.js
+++ 7_color-picker/src/Components/Color.js
@@ -1,6 +1,10 @@
-export default function Color({hex, name}) {
+export default function Color({hex, name, setBackgroundColor}) {
     return (
-        <button className='color-square' style='{{backgroundColor: hex}}'>
+        <button
+            className='color-square'
+            style={{backgroundColor: hex}}
+            onClick={() => setBackgroundColor(hex)}
+        >
             <h2>{name}</h2>
         </button>
     );
